3251dfc3e7f0e3475e3924badf9bd999da8e7e66
yuming chen <mathcym@outlook.com>
2020-11-13 13:25:44 +0800

    [yuming chen] finish test.

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,24 @@
+import Axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    const response = { data: { username: "max", password: "?!cym2293" } };
+    Axios.post.mockResolvedValue(response);
+    await expect(register("max", "?!cym2293")).resolves.toEqual({
+      username: "max",
+      password: "?!cym2293",
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+    await expect(register("max", "?!cym2293")).rejects.toThrow(Error);
   });
 });
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    const users = [{ name: "Max" }];
+    const reps = { data: users };
+
+    axios.get.mockImplementation(() => Promise.resolve(reps));
+
+    return getUsers().then((data) => expect(data).toEqual(users));
   });
 });
